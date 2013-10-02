678ed97650025a42bbb5e4ef3d424f7cd74efb0c
resteve <resteve@zikzakmedia.com>
2013-10-02 17:50:13 +0200

    Header py files

--- report.py
+++ report.py
@@ -1,3 +1,6 @@
+# This file is part of account_financial_statement module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
 from trytond.model import ModelView, ModelSQL, Workflow, fields
 from trytond.transaction import Transaction
 from trytond.pyson import Eval
--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,4 @@
-#This file is part of the account_financial_statement module for Tryton.
-#The COPYRIGHT file at the top level of
-#this repository contains the full copyright notices and license terms.
-
+#This file is part account_financial_statement module for Tryton.
+#The COPYRIGHT file at the top level of this repository contains
+#the full copyright notices and license terms.
 from .test_account_financial_statement import suite
