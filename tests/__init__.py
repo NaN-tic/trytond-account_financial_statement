46de55f964bc0a4eae1b09be1d4445162d341e39
resteve <resteve@zikzakmedia.com>
2013-10-25 15:19:57 +0200

    Pyflakes

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,3 +2,5 @@
 #The COPYRIGHT file at the top level of this repository contains
 #the full copyright notices and license terms.
 from .test_account_financial_statement import suite
+
+__all__ = ['suite']
