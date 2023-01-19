e86d49b81235422b40c5c355aaf2edd16a932417
Jared Esparza <jared.esparza@nan-tic.com>
2023-01-19 14:03:34 +0100

    Migrate to HTML Report | #072745

---
+++ financial_statement_report/financial_statement.py
@@ -0,0 +1,6 @@
+from trytond.modules.html_report.html_report import HTMLReport
+
+
+class FinancialStatementReport(HTMLReport):
+    'Financial Statement Report'
+    __name__ = 'account.financial.statement.report'
